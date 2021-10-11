ce1c2cb8d2219ad2e7e911dd3aa6ab7a7353b9a1
Homie <ghals0708@gmail.com>
2021-10-11 20:53:55 +0900

    Use Memo

--- habit-tracker/src/components/habbitAddForm.jsx
+++ habit-tracker/src/components/habbitAddForm.jsx
@@ -1,25 +1,22 @@
-import React, { PureComponent } from 'react';
+import React, { memo } from 'react';
 
-class HabbitAddForm extends PureComponent {
-    intputRef = React.createRef();
-    formRef = React.createRef();
-    onSubmit = event =>{
+const HabbitAddForm =  memo(props =>{
+    const intputRef = React.createRef();
+    const formRef = React.createRef();
+    const onSubmit = event =>{
         event.preventDefault();
-        const name = this.intputRef.current.value;
-        name && this.props.onAdd(name);
+        const name = intputRef.current.value;
+        name && props.onAdd(name);
         //this.intputRef.current.value = '';
-        this.formRef.current.reset();
+        formRef.current.reset();
+    }
 
-    }
-    render() {
-        console.log('habbitAddForm');
-        return (
-            <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
-                <input ref={this.intputRef} type="text" placeholder="Habbit" className="add-input"/>
-                <button type="submit" className="add-button">Add</button>
-            </form>
-        );
-    }
-}
+    return (
+        <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+            <input ref={intputRef} type="text" placeholder="Habbit" className="add-input"/>
+            <button type="submit" className="add-button">Add</button>
+        </form>
+    );
+});
 
 export default HabbitAddForm;
