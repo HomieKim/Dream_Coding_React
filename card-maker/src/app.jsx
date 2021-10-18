4f438761b48d41b8de53b28e203495819f4e70f6
Homie <ghals0708@gmail.com>
2021-10-18 18:08:01 +0900

    setting

---
+++ card-maker/src/app.jsx
@@ -0,0 +1,9 @@
+
+import './app.css';
+import LogIn from './components/login/logIn';
+
+function App() {
+  return  <LogIn></LogIn>
+}
+
+export default App;
---
+++ card-maker/src/components/login/logIn.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const LogIn = (props) => (
+            <div>
+            <header>Business Card Maker</header>
+            <section>
+                <div>LogIn</div>
+                <div>
+                    Google
+                </div>
+                <div>
+                    Github
+                </div>
+            </section>
+            <footer>Code your dream</footer>
+            </div>
+
+    );
+
+export default LogIn;
