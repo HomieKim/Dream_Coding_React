00176100ec8db0d0be5495b5a2f1ce7e5f24083e
HomieKim <ghals0708@gmail.com>
2021-11-23 21:37:08 +0900

    add edit preview

---
+++ card-maker/src/components/preview/preview.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './preview.module.css'
+
+
+const preview = ()=>{
+    return (
+       <section className={styles.preview}>
+           <h1 className={styles.title}>preview</h1>
+       </section>
+    )
+}
+
+export default preview;
