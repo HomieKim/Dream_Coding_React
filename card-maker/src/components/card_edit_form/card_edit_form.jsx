737216332913696f76c97f4a9b9f6832228caf35
HomieKim <ghals0708@gmail.com>
2021-11-25 22:54:30 +0900

    css 수정

--- card-maker/src/components/card_edit_form/card_edit_form.jsx
+++ card-maker/src/components/card_edit_form/card_edit_form.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import styles from './card_edit_form.module.css';
-const CardEditForm = ({card})=> {
-    return (
-        <h1>{card.id}</h1>
-    )
-}
+import React from "react";
+import styles from "./card_edit_form.module.css";
+const CardEditForm = ({ card }) => {
+  const { name, school, title, email, message, theme, fileName, fileURL } =card;
+  return (
+    <from>
+      <input type="text" name="name" value={name} />
+      <input type="text" name="school" value={school} />
+      <input type="text" name="title" value={title} />
+      <input type="text" name="email" value={email} />
+    </from>
+  );
+};
 
 export default CardEditForm;
